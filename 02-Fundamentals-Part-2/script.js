1f48d6ce6eae0d2e5dc2159041a4a3b097afcf7a
Precatsunbeam <yogeshchavan113@gmail.com>
2024-05-17 10:06:55 +0530

    Fundamental-2

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -125,7 +125,7 @@ console.log(age1,age2,age3);
 
 const age = [calcAge(year[0]), calcAge(year[1]), calcAge(year[2])];
 console.log(age);
-*/ 
+
  
 const frnds = ['Ram', 'Sham', 'Tejas'];
 
@@ -159,4 +159,163 @@ console.log(frnds.includes(23));
 
 if(frnds.includes('Ram')){
     console.log('You have frnd called Ram');
+}
+
+
+const yogeshArray = ['Yogesh', 'Chavan', '1996', 'Devloper', ['Ram', 'Sham', 'Tejas']];
+
+const yogesh = {
+    firstName: 'Yogesh',
+    lastName: 'Chavan',
+    age: 1996,
+    job: 'Devloper',
+    frnds: ['Ram', 'Sham', 'Tejas']
+};
+console.log(yogesh);
+console.log(yogesh.frnds);
+console.log(yogesh['frnds']);
+
+const nameKey = 'Name';
+console.log(yogesh['first'+ nameKey]);
+console.log(yogesh['last'+ nameKey]);
+
+const interstedIn= prompt('What do you want? choose between firstName, lastName, age, job & frnds');
+
+if(yogesh[interstedIn]){
+    console.log(yogesh[interstedIn]);
+}else{
+    console.log('Wrong request');
+}
+
+yogesh.location = 'Malvan';
+console.log(yogesh);
+
+console.log(`${yogesh.firstName} has ${yogesh.frnds.length}, and his best frnd is called ${yogesh.frnds[0]}`);
+ 
+
+const yogesh = {
+    firstName: 'Yogesh',
+    lastName: 'Chavan',
+    birthYear: 1996,
+    job: 'Devloper',
+    frnds: ['Ram', 'Sham', 'Tejas'],
+    hasDriversLicense: true,
+
+    // calcAge: function(){
+    //     console.log(this);
+    //     return 2024 -this.birthYear;
+    // }
+
+    calcAge: function(){
+        this.age =  2024 -this.birthYear;
+        return this.age;
+    },
+
+    getsSummary: function(){
+        return `${this.firstName} is a ${this.calcAge()} year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+};
+console.log(yogesh.calcAge());
+// console.log(yogesh['calcAge'](1996));
+
+console.log(yogesh.getsSummary());
+
+
+
+
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    
+    calcBMI: function(){
+    this.bmi = this.mass / this.height **2;
+    return this.bmi;
+    }
+};
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    
+    calcBMI: function(){
+    this.bmi = this.mass / this.height **2;
+    return this.bmi;
+    }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+if(mark.bmi > john.bmi){
+    console.log(`${mark.fullName}'s BMI (${mark.bmi.toFixed(1)}) is higher than ${john.fullName}'s (${john.bmi.toFixed(1)})!`);
+}
+else if(john.bmi > mark.bmi){
+    console.log(`${john.fullName}'s BMI (${john.bmi.toFixed(1)}) is higher than ${mark.fullName}'s (${mark.bmi.toFixed(1)})!`);
+}
+else{
+    console.log(`${mark.fullName} and ${john.fullName} have the same BMI (${mark.bmi.toFixed(1)})!`);
+}
+
+for(let rep = 1; rep<=10; rep++){
+    console.log(`Lifting weight repitations 🏋️‍♂️ ${rep}`);
+}
+
+
+const yogesh = ['Yogesh', 'Chavan', '1996', 'Devloper', ['Ram', 'Sham', 'Tejas'],true];
+
+const types = [];
+
+for(let i=0; i<yogesh.length; i++){
+    console.log(yogesh[i], typeof yogesh[i]);
+
+    // types[i] = typeof yogesh[i];
+    types.push(typeof yogesh[i]);
+}
+
+console.log(types);
+
+const years = [1996,1999,2002,2005];
+const ages= [];
+for(let i=0; i<years.length; i++){
+    ages.push(2024-years[i]);
+}
+console.log(ages);
+
+
+const yogesh = ['Yogesh', 'Chavan', '1996', 'Devloper', ['Ram', 'Sham', 'Tejas'],true];
+
+for(let i = yogesh.length -1; i>= 0; i--){
+    console.log(i, yogesh[i]);
+}
+
+for(let exercise = 1; exercise <=3; exercise++){
+    console.log(`Start exercise ${exercise}`);
+
+    for (let rep =1; rep<=6; rep++){
+        console.log(`Exercise ${exercise}: Lifting weight 🏋️‍♂️ repitition ${rep}`);
+    }
+}
+
+
+for (let rep =1; rep<=6; rep++){
+    console.log(`Lifting weight 🏋️‍♂️ repitition ${rep}`);
+}
+let rep = 1;
+while(rep <= 10){
+    console.log(`Lifting weight 🏋️‍♂️ repitition ${rep}`);
+    rep++;
+} 
+*/
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while(dice !== 6){
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice === 6){
+        console.log('Loop is end...');
+    }
 }
