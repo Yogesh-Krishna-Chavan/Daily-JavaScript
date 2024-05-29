ca008720f875078fc47f24716a2f8b9bc80fd461
Precatsunbeam <yogeshchavan113@gmail.com>
2024-05-29 21:33:48 +0530

    Guess My Number Project Complete

--- 05-Guess_My_Number/script.js
+++ 05-Guess_My_Number/script.js
@@ -10,21 +10,35 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const SecretNumber = Math.trunc(Math.random() * 20) + 1;
+let SecretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-
-document.querySelector('.number').textContent = SecretNumber;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
-  } else if (guess === SecretNumber) {
+  } 
+  
+  //when player wins
+  else if (guess === SecretNumber) {
     document.querySelector('.message').textContent = '🏆 Correct Number!';
-  } else if (guess > SecretNumber) {
+    document.querySelector('.number').textContent = SecretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if(score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore'). textContent = highScore
+    }
+  } 
+
+  //when input is too high
+  else if (guess > SecretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High Number!';
       score--;
@@ -33,7 +47,10 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '👎 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < SecretNumber) {
+  } 
+
+  //when input is too low
+  else if (guess < SecretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low Number!';
       score--;
@@ -43,4 +60,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+  score =20;
+  SecretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').textContent = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
