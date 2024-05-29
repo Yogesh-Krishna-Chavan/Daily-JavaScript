e1599782dad1a3bdc13724072553d845c583cd06
Precatsunbeam <yogeshchavan113@gmail.com>
2024-05-29 10:00:10 +0530

    Guess My Number Project JS write

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,3 +1,46 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+const SecretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+document.querySelector('.number').textContent = SecretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === SecretNumber) {
+    document.querySelector('.message').textContent = '🏆 Correct Number!';
+  } else if (guess > SecretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High Number!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👎 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < SecretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low Number!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👎 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
