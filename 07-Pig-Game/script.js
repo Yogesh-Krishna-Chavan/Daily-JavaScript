9a3a4ffd134bef84dbc3160cb96bb55ffb1b935f
Precatsunbeam <yogeshchavan113@gmail.com>
2024-05-31 09:43:23 +0530

    Pig Game

---
+++ 07-Pig-Game/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//Selecting elemnets
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceE1 = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceE1.classList.add('hidden');
