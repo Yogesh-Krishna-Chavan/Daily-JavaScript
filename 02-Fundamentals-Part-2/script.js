7f101c098d33b3ad6b135a4eb5a60cde1468effd
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-05-31 14:56:21 +0530

    Dice Game

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -307,7 +307,7 @@ while(rep <= 10){
     console.log(`Lifting weight 🏋️‍♂️ repitition ${rep}`);
     rep++;
 } 
-*/
+
 
 let dice = Math.trunc(Math.random() * 6) + 1;
 console.log(dice);
@@ -319,3 +319,36 @@ while(dice !== 6){
         console.log('Loop is end...');
     }
 }
+*/
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
+  /* Write your code below. Good luck! 🙂 */
+  
+  const bills = [22,295,176,440,37,105,10,1100,86,52];
+  
+  const tips = [];
+  const totals = [];
+  
+  for(let i=0; i< bills.length; i++){
+      const tip = calcTip(bills[i]);
+      tips.push(tip);
+      totals.push(bills[i] + tip);
+  }
+  
+  console.log('Bills: ', bills);
+  console.log('Tips: ', tips);
+  console.log('totals: ', totals);
+  
+  const calcAverage = (arr) => {
+      let sum = 0;
+      for (let i = 0; i < arr.length; i++) {
+          sum += arr[i];
+      }
+      return sum / arr.length;
+  };
+  
+  const averageTotal = calcAverage(totals);
+  console.log('Average Total:', averageTotal);
