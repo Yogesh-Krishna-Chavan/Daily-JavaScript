2ba2f3ab496cd858457cf7944ca08c41ac61dfad
Precatsunbeam <yogeshchavan113@gmail.com>
2024-06-02 01:18:57 +0530

    Behind working of JS 1st trial

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `You are  a millenial, ${firstName}`;
+      console.log(str);
+    }
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Yogesh';
+calcAge(1996);
