491c96f74ae921e866801cd8b741ce6bb34bf790
Precatsunbeam <yogeshchavan113@gmail.com>
2024-06-07 00:06:32 +0530

    Data Structure

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -27,24 +27,21 @@ const restaurant = {
     },
   },
 
-  order: function(starterIndex, mainIndex){
-    return[this.starterMenu[starterIndex],this.mainMenu[mainIndex]];
-  }
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
 
-
-const arr = [2,3,4];
+const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
-const [x,y,z] = arr;
-console.log(x,y,z);
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
 
-// const [first, second] = restaurant.categories;
-// console.log(first,second);
-
-let [main, secodary] = restaurant.categories;
+let [main, , secodary] = restaurant.categories;
 console.log(main, secodary);
 
 //Switching variables
@@ -55,6 +52,19 @@ console.log(main, secodary);
 // console.log(main,secodary);
 
 [main, secodary] = [secodary, main];
-console.log(main,secodary);
+console.log(main, secodary);
 
-const [starter, main] = restaurant.order(2,0);
+//recieve 2 values from fuction
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//Default values
+const [p, q = 1, r = 2] = [8, 9];
+console.log(p, q, r);
