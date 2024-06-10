f94d8daeba04920a688de958802b73ef0e4fb9d2
Precatsunbeam <yogeshchavan113@gmail.com>
2024-06-10 09:51:33 +0530

    Data Structure with operators

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -30,9 +30,45 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
 };
 
 /*
+const ingredients = [
+  prompt("let's make pasta...! ingredient 1?"),
+  prompt('Ingredient 2?'),
+  prompt('Ingredient 3?'),
+];
+
+//Objects
+restaurant.orderPasta(...ingredients);
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Yogesh' };
+console.log(newRestaurant);
+
+
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Pune',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
@@ -69,7 +105,7 @@ console.log(i, j, k);
 //Default values
 const [p, q = 1, r = 2] = [8, 9];
 console.log(p, q, r);
-*/
+
 
 const { name, openingHours, categories } = restaurant;
 console.log(name, openingHours, categories);
@@ -80,3 +116,77 @@ const {
   categories: tags,
 } = restaurant;
 console.log(restaurantName, hours, tags);
+
+//Default values
+const { menu = [], starterMenu: starer = [] } = restaurant;
+console.log(menu, starer);
+
+//Mutate varibales
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+//Nested Objects
+const {
+  fri: { open, close },
+} = openingHours;
+console.log(open, close);
+
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+const newMenu = [...restaurant.mainMenu, 'Puran-poli'];
+console.log(newMenu);
+
+//copy array
+const mainenuCopy = [...restaurant.mainMenu];
+
+//join 2 array
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+//Iterables: arrays,strings, maps, sets, Not objects
+const str = 'Yogesh';
+const letters = [...str, ' ', 's.'];
+console.log(letters);
+console.log(...str);
+*/
+
+//spread
+const arr = [1, 2, ...[3, 4]];
+
+//rest
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [Pizza, , Risotto, ...otherfood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(Pizza, Risotto, otherfood);
+
+//objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+//function
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+add(2, 3);
+
+const x = [23, 5, 7];
+add(...x);
