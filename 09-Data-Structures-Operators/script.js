b839c00c9f359d3c54cb495c0fcfbee44538e7d9
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-06-11 16:45:06 +0530

    Challenge complete

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -47,7 +47,6 @@ const restaurant = {
   },
 };
 
-/*
 const ingredients = [
   prompt("let's make pasta...! ingredient 1?"),
   prompt('Ingredient 2?'),
@@ -159,7 +158,7 @@ const str = 'Yogesh';
 const letters = [...str, ' ', 's.'];
 console.log(letters);
 console.log(...str);
-*/
+
 
 //spread
 const arr = [1, 2, ...[3, 4]];
@@ -190,3 +189,99 @@ add(2, 3);
 
 const x = [23, 5, 7];
 add(...x);
+*/
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 11.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//1.
+const [players1,players2] = game.players;
+console.log(players1,players2);
+
+//2
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+//3
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+//4
+const players1Final = [...players1, 'Ram', 'Sham', 'Jeet'];
+console.log(players1Final);
+
+//5
+const {odds: {team1, x: draw, team2}} = game;
+console.log(team1,team2,draw);
+
+//6
+const printGoals = function(...players){
+  console.log(`${players.length} goals were scored`)
+};
+printGoals('JHG', 'QWE', 'XYZ');
+printGoals('JHG', 'QWE', 'XYZ', 'ASD');
+
+printGoals(...game.score);
+
+//7
+team1 < team2 && console.log('Team1 is more likely to win');
+team2 < team1 && console.log('Team2 is more likely to win');
