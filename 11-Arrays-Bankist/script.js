2759fd38e775d762ccf56328d4c4e7e8799b616b
Precatsunbeam <yogeshchavan113@gmail.com>
2024-07-01 22:19:45 +0530

    New array methods

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -73,6 +73,7 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+/*
 /////////////////////////////////////////////////
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
@@ -102,3 +103,16 @@ console.log([...arr, ...arr2]);
 
 //Join
 console.log(letters.join(' - '));
+*/
+
+//At method
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//Getting last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('Yogesh'.at(-1));
