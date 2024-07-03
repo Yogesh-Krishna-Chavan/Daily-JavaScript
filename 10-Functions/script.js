c023367a564ed4ca46557cd0501c6411a7e4a673
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-07-03 15:23:20 +0530

    Function

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,40 @@
 'use strict';
+/*
+const bookings = [];
+
+const createBooking = function (flightNum, numPassenger, price){
+    const booking = {
+        flightNum, 
+        numPassenger, 
+        price,
+    };
+console.log(booking);
+bookings.push(booking);
+};
+
+createBooking('LH123');
+
+
+const flight = 'LH234';
+const yogesh = {
+    name: 'Yogesh Chavan',
+    passport: 24739479284,
+}
+
+
+const checkIn = function(flightNum, passenger){
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if(passenger.passport === 24739479284){
+        alert('Checked in');
+    }
+    else{
+        alert('Wrong passport!');
+    }
+};
+
+checkIn(flight, yogesh);
+console.log(flight);
+console.log(yogesh);
+*/
