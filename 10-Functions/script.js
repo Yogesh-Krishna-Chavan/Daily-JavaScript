4e7b63a01c5e2c7e879fc04906bf770dc8285e28
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-07-11 16:40:54 +0530

    Function calling

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -34,7 +34,52 @@ const checkIn = function(flightNum, passenger){
     }
 };
 
-checkIn(flight, yogesh);
-console.log(flight);
-console.log(yogesh);
-*/
+// checkIn(flight, yogesh);
+// console.log(flight);
+// console.log(yogesh);
+
+
+const newPassport = function(person){
+    person.passport = Math.trunc(Math.random()*100000000000);
+}
+
+newPassport(yogesh);
+checkIn(flight,yogesh);
+*/           
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function(str){
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher-order function
+const transform = function(str, fn){
+
+console.log(`Original string: ${str}`);
+console.log(`Transformed string: ${fn(str)}`);
+
+console.log(`Transformed by: ${fn.name}`);
+};
+transform('JavaScript is the fun!', upperFirstWord);
+
+const high5 = function() {
+    console.log('🖐️');
+};
+document.body.addEventListener('click',high5);
+['jonas', 'Martha', 'Adam'].forEach(high5);
+
+const greet = function(greeting){
+    return function(name){
+        console.log(`${greeting} ${name}`);
+    };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Yogesh');
+greeterHey('Chavan');
+
+greet('Hello')('Yogesh');
