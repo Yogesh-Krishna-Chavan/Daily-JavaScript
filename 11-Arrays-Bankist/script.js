8da228b7f23033af3910981f65d26a175c020cbb
Precatsunbeam <yogeshchavan113@gmail.com>
2024-07-25 20:08:28 +0530

    Array map & set

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -117,6 +117,7 @@ console.log(arr.at(-1));
 console.log('Yogesh'.at(-1));
 */
 
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for (const movement of movements) {
@@ -137,3 +138,25 @@ movements.forEach(function (movement, i, array) {
     console.log(`Movement ${i + 1} You are withdraw ${Math.abs(movement)}`);
   }
 });
+*/
+
+/*
+//Working with maps and sets
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+//Set
+const currenciesUnique = new Set(['USD', 'EUR', 'GBP']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+*/
+
