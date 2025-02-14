476688edf49d9046c1762cfabe19afb283057952
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2025-02-14 21:30:18 +0530

    try_catch.js

---
+++ 00-JS-Concept/try_catch_block.js
@@ -0,0 +1,17 @@
+//try, catch, finally block
+// try block is used to enclose the code that might throw an exception
+// catch block is used to handle the exception
+// finally block is used to execute the code after try and catch blocks regardless of the result
+
+function divide(a, b) {
+  try {
+    if (b === 0) {
+      throw new Error("Divide by zero not possible");
+    } else {
+      console.log(`a / b = ${a / b}`);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+divide(10, 0);
